b8de49bdfe65e8d7453973ade9232bd14ac8cc89
Colin Pollock <pollockcolin@gmail.com>
2020-02-14 22:17:41 -0800

    Updating data loading utilities to load IPA instead of Arpabet
    Previously I was using the standard ARPABET version, but I found a good IPA version with correct stress placement.

--- sonorous/pronunciationdata.py
+++ sonorous/pronunciationdata.py
@@ -2,31 +2,66 @@
 
 
 from collections import Counter
-from string import ascii_lowercase
-from typing import Set, Tuple
+from string import ascii_uppercase
+from typing import Dict, List, Set, Tuple
 
-import cmudict
 from pandas import DataFrame
 
 
-ALLOWABLE_LETTERS = set(ascii_lowercase)
-CMU = cmudict.dict()
+CMUDICT_FILEPATH = "data/cmudict-0.7b-ipa.txt"
+
+SYLLABIC_PHONEMES = {
+    "i",
+    "e",
+    "ʊ",
+    "o",
+    "u",
+    "ɑ",
+    "ɔ",
+    "ə",
+    "ɛ",
+    "ɪ",
+    "a",
+    "ɝ",
+    "æ",
+    "ʌ",
+}
+
+PRIMARY_STRESS = "ˈ"
+SECONDARY_STRESS = "ˌ"
+LONG_VOWEl = "ː"
+NON_PHONEME_SYMBOLS = {PRIMARY_STRESS, SECONDARY_STRESS, LONG_VOWEl}
+
+DIPHTHONGS = {
+    "oʊ",
+    "aʊ",
+    "aɪ",
+    "eɪ",
+    "ɔɪ",
+}
 
 
-def load_pronunciations() -> DataFrame:
+def load_pronunciations(num_rows: int = None) -> DataFrame:
     """Return a DataFrame with columns for `word` and `pronunciation`."""
-    records = []
-    for word, pronunciations in CMU.items():
-        if not all(letter in ALLOWABLE_LETTERS for letter in word):
-            continue
+    records: List[Dict[str, str]] = []
 
-        for pronunciation in pronunciations:
-            records.append({"word": word, "pronunciation": tuple(pronunciation)})
+    with open("t", "r") as fh:
+        for line in fh:
+            word, pronunciations = line.strip().split("\t")
 
-    pronunciations = DataFrame(records).set_index("word")
-    augment_pronunciations_df(pronunciations)
+            if not all(letter in ascii_uppercase for letter in word):
+                continue
 
-    return pronunciations
+            for pronunciation in pronunciations.split(","):
+                if num_rows is not None and len(records) > num_rows:
+                    break
+                records.append(
+                    {"word": word.lower(), "pronunciation": pronunciation.strip()}
+                )
+    pronunciations_df = DataFrame.from_records(records).set_index("word")
+    augment_pronunciations_df(pronunciations_df)
+
+    return pronunciations_df
 
 
 def augment_pronunciations_df(pronunciations: DataFrame) -> None:
@@ -36,7 +71,7 @@ def augment_pronunciations_df(pronunciations: DataFrame) -> None:
     - num_phonemes
     - num_syllables
     """
-    pronunciations["num_phonemes"] = pronunciations.pronunciation.apply(len)
+    pronunciations["num_phonemes"] = pronunciations.pronunciation.apply(count_phonemes)
     pronunciations["num_syllables"] = pronunciations.pronunciation.apply(
         count_syllables
     )
@@ -46,38 +81,33 @@ def augment_pronunciations_df(pronunciations: DataFrame) -> None:
     ] = pronunciations.pronunciation.apply(count_primary_stressed_syllables)
 
 
-def get_syllabic_phonemes() -> Set[str]:
-    """Return a set of all phonemes that are syllabic.
-
-    This is just vowels and the syllabic /R/, /ER/. CMU Dict doesn't seem to have other syllabic consonants.
-    """
-    stresses = ("0", "1", "2")
-    vowels = set()
-    for phoneme, features in cmudict.phones():
-        if "vowel" in features:
-            for stress in stresses:
-                vowels.add(phoneme + stress)
-
-    return vowels
+def count_phonemes(pronunciation: str) -> int:
+    pronunciation = _remap_diphthongs(pronunciation)
+    return sum(1 for phoneme in pronunciation if phoneme not in NON_PHONEME_SYMBOLS)
 
 
-SYLLABIC_PHONEMES = get_syllabic_phonemes()
-
-
-def count_syllables(pronunciation: Tuple[str, ...]) -> int:
+def count_syllables(pronunciation: str) -> int:
     """Return the number of syllables in a single pronunciation.
 
     This is approximate as I'm assuming the number of syllabic phonemes is the
     same as the number of vowels. It should be right almost all the time though.
     """
-    counts = Counter(pronunciation)
-    return sum(counts[vowel] for vowel in SYLLABIC_PHONEMES)
+    counts = Counter(_remap_diphthongs(pronunciation))
+    return sum(counts[phoneme] for phoneme in SYLLABIC_PHONEMES)
 
 
-def count_primary_stressed_syllables(pronunciation: Tuple[str, ...]) -> int:
+def count_primary_stressed_syllables(pronunciation: str) -> int:
     """Return the number of syllables with primary stress.
 
     Typically words only have one syllable with primary stress, but compound words or acronyms will
     break that rule. For example, "ai" is /EY1 AY1/ because it's said as two words.
     """
-    return sum(1 for phoneme in pronunciation if phoneme.endswith("1"))
+    return sum(1 for phoneme in pronunciation if phoneme == PRIMARY_STRESS)
+
+
+def _remap_diphthongs(pronunciation: str) -> str:
+    """Map each pronunciation to its first vowel."""
+    for diphthong in DIPHTHONGS:
+        pronunciation = pronunciation.replace(diphthong, diphthong[0])
+
+    return pronunciation
--- tests/test_pronunciationdata.py
+++ tests/test_pronunciationdata.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from sonorous.pronunciationdata import (
     SYLLABIC_PHONEMES,
     augment_pronunciations_df,
+    count_phonemes,
     count_primary_stressed_syllables,
     count_syllables,
     load_pronunciations,
@@ -13,44 +14,37 @@ def test_load_data():
     """This is partially just an integregation test to make sure the CMU Dict data
     doesn't change from under my feet.
     """
-    df = load_pronunciations()
-    assert len(df) == 125801
-    cat = df.loc["cat"]
-    assert cat.pronunciation == ("K", "AE1", "T")
+    pronunciations = load_pronunciations()
+    assert len(pronunciations) == 124567
+    cat = pronunciations.loc["cat"]
+
+    assert cat.pronunciation == "ˈkæt"
     assert cat.num_phonemes == 3
     assert cat.num_syllables == 1
     assert cat.num_primary_stressed_syllables == 1
 
 
-def test_get_all_syllabic_phonemes():
-    """The tested function mostly depends on the cmudict, so rather than testing its contents
-    thoroughly I'll do a few sanity checks.
-    """
-    syllabic_phonemes = ("AH0", "AH1", "AH2", "ER0", "ER1", "ER2")
-    non_syllabic_phonemes = ("K", "SH", "M")
-
-    for syllabic in syllabic_phonemes:
-        assert syllabic in SYLLABIC_PHONEMES
-
-    for non_syllabic in non_syllabic_phonemes:
-        assert non_syllabic not in SYLLABIC_PHONEMES
-
-
 def test_count_syllables():
-    assert count_syllables(("K", "AH1", "L", "IH0", "N")) == 2
-    assert count_syllables(("F", "ER1")) == 1
+    assert count_syllables("ˈdɔgi") == 2
+    assert count_syllables("rɪˈpiːtɝ") == 3
+    assert count_syllables("əˈbaʊt") == 2
+
+
+def test_count_phonemes():
+    assert count_phonemes("ˈfɝːi") == 3
+    assert count_phonemes("əˈbaʊt") == 4
 
 
 def test_count_primary_stressed_syllables():
-    assert count_primary_stressed_syllables(("K", "AE1", "F", "ER0")) == 1
-    assert count_primary_stressed_syllables(("EY1", "AY1")) == 2
+    assert count_primary_stressed_syllables("ˈkæt") == 1
+    assert count_primary_stressed_syllables("ˈeɪˈaɪ") == 2
 
 
 def test_augment_pronunciations_df():
-    pronunciations = DataFrame({"pronunciation": [("R", "EH1", "DJ", "IY0")]})
+    pronunciations = DataFrame({"pronunciation": ["ˈrɛdʒi"]})
     augment_pronunciations_df(pronunciations)
 
     reggie = pronunciations.iloc[0]
-    assert reggie["num_phonemes"] == 4
+    assert reggie["num_phonemes"] == 5
     assert reggie["num_syllables"] == 2
 
